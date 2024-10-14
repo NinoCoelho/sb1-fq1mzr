51c385d17c37f922b6ea6352c3d04cd23e5ed643
NinoCoelho <coelho.idemir@gmail.com>
2024-10-14 12:39:08 -0400

    firebase

--- src/firebase.ts
+++ src/firebase.ts
@@ -4,12 +4,14 @@ import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
   // Replace with your Firebase configuration
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  
+  apiKey: "AIzaSyDYBQKq4-dCi2KRbOiT3QFHCNHNfJFYdZ4",
+  authDomain: "pacientspoc.firebaseapp.com",
+  projectId: "pacientspoc",
+  storageBucket: "pacientspoc.appspot.com",
+  messagingSenderId: "868402602574",
+  appId: "1:868402602574:web:ac92c55e4e2b7b16ebd91f",
+  measurementId: "G-B6EB1G7LFW"
 };
 
 const app = initializeApp(firebaseConfig);
